98394e769b3df1744c310afd02733b49212da165
Ben Petrosky and Jesse Hoare Evans <ben+jesse@>
2017-05-24 16:12:13 -0700

    make app work

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,13 +1,26 @@
-// var apiKey = "";
 var Bike = require('./../js/bike.js').bikeModule;
 
+var bikeInfo = function(bikes){
+  $('.info').text('');
+  bikes.forEach(function(bike) {
+    $('.info').append('<li><ul><li>Color:' + bike.color + '</li><li>Year: ' + bike.year + '</li><li>Picture: <img src="' + bike.image + '"</li>');
+  });
+};
+
 $(document).ready(function(){
+  var bikeSearch = new Bike();
   $('#location-submit').click(function(){
     var city = $('#location').val();
     $('#location').val("");
-    $('.info').text("The city you have chosen is " + city + ".");
-    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=https://bikeindex.org/api/v3/swagger_doc?q=' + city, function(response){
-  console.log(response);
-    });
+    bikeSearch.getBike(city, bikeInfo);
+
   });
 });
+
+//     $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=Seattle,WA&distance=10&stolenness=proximity&access_token=https://bikeindex.org/api/v3/swagger_doc?q=' + city, function(response){
+//     $('.info').text("The city you have chosen is " + city + ".  We also know  " + response.bikes);
+//
+//     console.log(response);
+//     });
+//   });
+// });
--- js/bike.js
+++ js/bike.js
@@ -1,10 +1,23 @@
-Bike = function(){
+var appid = "02b6cd61f97488e3d720c0e0f614a6fa207faf0f52b6234a31357af5961295ce";
+
+function Bike(color, year, image){
+  this.color = color;
+  this.year = year;
+  this.image = image;
+}
+
+Bike.prototype.getBike = function(city, bikeInfo) {
+  bikes = [];
+$.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=10&stolenness=proximity&appid=' + appid).then(function(response){
+  for (var i=0; i < 25; i++) {
+    var color = response.bikes[i].frame_colors;
+    var year = response.bikes[i].year;
+    var image = response.bikes[i].thumb;
+    newBike = new Bike(color, year, image);
+    bikes.push(newBike);
+    }
+    bikeInfo(bikes);
+  });
 };
 
-Bike.prototype.getBike = function(city, displayYear) {
-$.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&access_token=https://bikeindex.org/api/v3/swagger_doc?q=' + city).then(function(response){
-      console.log(response);
-      displayYear(response.bike.year);
-});
-};
 exports.bikeModule = Bike;
